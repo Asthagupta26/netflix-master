6ea473c463da60bca71ab81f20f4c1ddace7288f
Astha gupta <guptaastha977@gmail.com>
2023-10-22 15:17:01 +0530

    Netflix Clone

---
+++ netflix-master/src/App.js
@@ -0,0 +1,17 @@
+import "./App.scss";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./Components/Home/Home";
+import Header from "./Components/Header/Header";
+
+function App() {
+  return (
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
